d494a368a3658eaa749641b4e535d47fe4f80924
Letícia Alves <85093388+Leticiaa7@users.noreply.github.com>
2022-10-06 11:12:50 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide:'true',
+    fade:'true',
+    grabCursor:'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets:true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    
+    /*
+    breakpoints(){
+      0: {
+        slidesPerview: 3,
+      },
+      520: {
+        slidesPerview: 2,
+      },
+      950: {
+        slidesPerview: 1,
+      },
+    },
+    */
+  });
